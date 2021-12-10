7eac9f3e306ecb60ff65a266ec435c55fc465259
alexandresantosm <amenezes031@gmail.com>
2021-12-10 16:03:33 -0300

    feat: add Transaction model

---
+++ api/src/entity/Transaction.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  value: number;
+
+  @ManyToOne(() => User, user => user.id)
+  @JoinColumn()
+  requestUser: User;
+
+  @ManyToOne(() => User, user => user.id, { nullable: true })
+  payingUser: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
