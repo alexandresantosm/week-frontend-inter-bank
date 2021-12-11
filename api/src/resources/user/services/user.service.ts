13bdd02e9bef2e2ce7af7663f77cdf368ee19eab
alexandresantosm <amenezes031@gmail.com>
2021-12-11 11:22:47 -0300

    feat: endpoint retun access token

--- api/src/resources/user/services/user.service.ts
+++ api/src/resources/user/services/user.service.ts
@@ -1,8 +1,10 @@
 import { AppError } from '../../../shared/error/AppError';
 import { encode } from '../../../utils/Hash';
+import { generateToken } from '../../../utils/Token';
 import { UserSignInDTO, UserSignUpDTO } from '../dtos';
 import { getRepository } from 'typeorm';
 import { User } from '../../../entity';
+import { authConfig } from '../../../config/auth';
 
 export class UserService {
   async signin(user: UserSignInDTO) {
@@ -11,6 +13,7 @@ export class UserService {
     const passwordHash = encode(password);
 
     const existUser = await userRepository.findOne({
+      relations: ['account'],
       where: { email, password: passwordHash },
     });
 
@@ -18,7 +21,25 @@ export class UserService {
       throw new AppError('Usuário não encontrado', 401);
     }
 
-    return existUser;
+    const { secret, expiresIn } = authConfig.jwt;
+    const { id, firstName, lastName, account } = existUser;
+    const body = {
+      firstName,
+      lastName,
+      accountNumber: account.number,
+      accountDigit: account.digit,
+      wallet: account.wallet,
+    };
+    const validate = {
+      subject: id,
+      expiresIn,
+    };
+    const token = generateToken(body, secret, validate);
+
+    // @ts-expect-error ignore
+    delete existUser.password;
+
+    return { accessToken: token };
   }
 
   async signup(user: UserSignUpDTO) {}
