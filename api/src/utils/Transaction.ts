27c4a705348ec68a2a1c28db413adc764582068b
alexandresantosm <amenezes031@gmail.com>
2021-12-13 10:50:24 -0300

    feat: add encode and decode key

---
+++ api/src/utils/Transaction.ts
@@ -0,0 +1,22 @@
+import { encode, decode } from 'js-base64';
+
+export const encodeKey = (
+  userId: string,
+  value: number,
+  registerId: string,
+) => {
+  const part1 = encode(userId);
+  const part2 = encode(value.toString());
+  const part3 = encode(registerId);
+
+  return `${part1}-${part2}-${part3}`;
+};
+
+export const decodeKey = (key: string) => {
+  const [part1, part2, part3] = key.split('-');
+  const userId = decode(part1);
+  const value = decode(part2);
+  const registerId = decode(part3);
+
+  return { userId, value, registerId };
+};
