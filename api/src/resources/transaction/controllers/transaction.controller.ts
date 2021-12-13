fa47047748534e8fbc6d80f48c7e495ccf41c9d9
alexandresantosm <amenezes031@gmail.com>
2021-12-13 12:47:40 -0300

    feat: transaction controller

--- api/src/resources/transaction/controllers/transaction.controller.ts
+++ api/src/resources/transaction/controllers/transaction.controller.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from 'express';
+import { TransactionService } from '../services/transaction.service';
 
 export class TransactionController {
   async request(req: Request, res: Response) {
-    res.send('Generate key PIX');
+    const transactionService = new TransactionService();
+    const { value } = req.body;
+    const user = req.user;
+    const requestKey = await transactionService.request(value, user);
+    return res.status(201).send({ copyPasteKey: requestKey });
   }
 
   async pay(req: Request, res: Response) {
-    res.send('Paying with PIX');
+    const transactionService = new TransactionService();
+    const { key } = req.params;
+    const user = req.user;
+    const payment = await transactionService.pay(key, user);
+    return res.status(201).send(payment);
   }
 
   async transactions(req: Request, res: Response) {
-    res.send('Show all user transactions');
+    const transactionService = new TransactionService();
+    const user = req.user;
+    const transactions = await transactionService.transactions(user);
+    return transactions;
   }
 }
