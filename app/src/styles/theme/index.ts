62cdf7dad2218ff4398be61ecfb999a28c70d8f2
alexandresantosm <amenezes031@gmail.com>
2021-12-14 15:27:41 -0300

    feat: create theme default

---
+++ app/src/styles/theme/index.ts
@@ -0,0 +1,11 @@
+import { COLORS } from "../colors";
+
+export const theme = {
+  colors: {
+    primary: COLORS.PRIMARY,
+    secondary: COLORS.SECONDARY,
+    tertiary: COLORS.TERTIARY,
+    background: COLORS.BACKGROUND,
+    backgroundLight: COLORS.BACKGROUND_LIGHT,
+  },
+};
