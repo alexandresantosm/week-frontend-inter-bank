d836357398f601468434d65dac045768d78e830a
alexandresantosm <amenezes031@gmail.com>
2021-12-14 15:29:50 -0300

    feat: add theme and global styles to pages

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <div>App</div>;
-}
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/globalStyles";
+import { theme } from "./styles/theme";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
 
 export default App;
