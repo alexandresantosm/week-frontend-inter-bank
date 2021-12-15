f225cab23edca7bd7abe719400bca097b1e6e2c0
alexandresantosm <amenezes031@gmail.com>
2021-12-15 17:43:33 -0300

    feat: finished login page with e-mail and password input and button

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 import { Background, ButtonContainer, InputContainer, Wrapper } from "./styles";
 
 import BackgroundImage from "../../assets/images/background-login.jpg";
-import { Card, Logo } from "../../components";
+import { Card, Logo, Input, Button } from "../../components";
 
 export const SignIn = () => {
   return (
@@ -9,6 +11,16 @@ export const SignIn = () => {
       <Background image={BackgroundImage} />
       <Card width="25.5rem">
         <Logo alt="Logo Inter" width={172} height={61} />
+        <InputContainer>
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" type="password" />
+        </InputContainer>
+        <ButtonContainer>
+          <Button type="button">Entrar</Button>
+          <p>
+            Ainda não é cadastrado? <Link to="/signup">Cadrastre-se Já</Link>
+          </p>
+        </ButtonContainer>
       </Card>
     </Wrapper>
   );
