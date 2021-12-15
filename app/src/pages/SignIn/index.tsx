c452aecfb289aacc5eaca738a2a0de356d4f114c
alexandresantosm <amenezes031@gmail.com>
2021-12-14 21:29:55 -0300

    feat: add Card component and rename import bacground to the SignIn page

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,11 +1,13 @@
 import { Background, ButtonContainer, InputContainer, Wrapper } from "./styles";
 
-import background from "../../assets/images/background-login.jpg";
+import BackgroundImage from "../../assets/images/background-login.jpg";
+import { Card } from "../../components";
 
 export const SignIn = () => {
   return (
     <Wrapper>
-      <Background image={background} />
+      <Background image={BackgroundImage} />
+      <Card width="25.5rem">Inter</Card>
     </Wrapper>
   );
 };
