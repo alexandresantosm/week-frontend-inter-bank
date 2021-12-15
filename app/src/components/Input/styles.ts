d74b9abdb2a5f94f38ab26e5f78bf713befca8c2
alexandresantosm <amenezes031@gmail.com>
2021-12-15 17:40:48 -0300

    feat: fixed bug

--- app/src/components/Input/styles.ts
+++ app/src/components/Input/styles.ts
@@ -10,15 +10,17 @@ export const InputContainer = styled.div`
   display: flex;
   justify-content: center;
   z-index: 5000;
+  &:focus-within {
+    border: 1px solid #8dc63f;
+  }
 `;
 
 export const InputContent = styled.input`
-  input {
-    width: 100%;
-    font-size: 0.75rem;
-    font-weight: 400;
-    background-color: transparent;
-    border: 0;
-    margin: 0 1.25rem;
-  }
+  width: 100%;
+  font-size: 0.75rem;
+  font-weight: 400;
+  background-color: transparent;
+  border: 0;
+  margin: 0 1.25rem;
+  outline: none;
 `;
