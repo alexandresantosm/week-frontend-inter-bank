e4f6a416fc5b3bfecd778472de9db94f762dd6ea
alexandresantosm <amenezes031@gmail.com>
2021-12-14 21:33:10 -0300

    feat: add Logo component to the SignIn page

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,13 +1,15 @@
 import { Background, ButtonContainer, InputContainer, Wrapper } from "./styles";
 
 import BackgroundImage from "../../assets/images/background-login.jpg";
-import { Card } from "../../components";
+import { Card, Logo } from "../../components";
 
 export const SignIn = () => {
   return (
     <Wrapper>
       <Background image={BackgroundImage} />
-      <Card width="25.5rem">Inter</Card>
+      <Card width="25.5rem">
+        <Logo alt="Logo Inter" width={172} height={61} />
+      </Card>
     </Wrapper>
   );
 };
