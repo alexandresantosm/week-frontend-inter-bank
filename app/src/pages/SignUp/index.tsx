730111ec82fe2f3b9305a1d4152d27bd55bbf5ea
alexandresantosm <amenezes031@gmail.com>
2021-12-15 21:59:12 -0300

    feat: create SignUp page

--- app/src/pages/SignUp/index.tsx
+++ app/src/pages/SignUp/index.tsx
@@ -1,3 +1,41 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import BackgroundImage from "../../assets/images/background-login.jpg";
+import { Card, Logo, Input, Button } from "../../components";
+
+import {
+  Background,
+  ButtonContainer,
+  InputContainer,
+  Wrapper,
+} from "../SignIn/styles";
+
 export const SignUp = () => {
-  return <div>Sign up</div>;
+  const navigate = useNavigate();
+  const handleToSignUp = () => {
+    navigate("/dashboard");
+  };
+  return (
+    <Wrapper>
+      <Background image={BackgroundImage} />
+      <Card width="25.5rem">
+        <Logo alt="Logo Inter" width={172} height={61} />
+        <InputContainer>
+          <Input placeholder="Nome" />
+          <Input placeholder="Sobenome" />
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" type="password" />
+          <Input placeholder="Confirmar senha" type="password" />
+        </InputContainer>
+        <ButtonContainer>
+          <Button type="button" onClick={handleToSignUp}>
+            Entrar
+          </Button>
+          <p>
+            Já possui uma conta? <Link to="/">Entre Já</Link>
+          </p>
+        </ButtonContainer>
+      </Card>
+    </Wrapper>
+  );
 };
