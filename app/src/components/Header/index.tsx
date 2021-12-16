dce6a2e6bebb762a80879adceee1b7747ea51818
alexandresantosm <amenezes031@gmail.com>
2021-12-16 12:30:08 -0300

    feat: add UserCircle in the Header and create logOff function

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,21 +1,28 @@
 import { useNavigate } from "react-router-dom";
 
-import { Logo } from "../Logo";
+import { Logo, UserCircle } from "../index";
 import { HeaderContainer, HeaderWrapper, UserInfo } from "./styles";
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const handleLogOff = () => {
+    navigate("/");
+  };
+
   return (
     <HeaderContainer>
       <HeaderWrapper>
         <Logo alt="Logo Inter" height={61} width={172} />
         <UserInfo>
-          circulo
+          <UserCircle initials="AS" />
           <div>
             <p>
               Olá, <span className="primary-color font-bold">Alexandre</span>
             </p>
             <strong>22001123-1</strong>
-            <a href="#">Sair</a>
+            <a href="#" onClick={handleLogOff}>
+              Sair
+            </a>
           </div>
         </UserInfo>
       </HeaderWrapper>
