fd2b16ffe1085c1ac2db66787ef874b6d815319d
alexandresantosm <amenezes031@gmail.com>
2021-12-21 20:32:13 -0300

    feat: add interceptor authorization

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: "http://localhost:3333",
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("@Inter:Token") || "";
+  config.headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  return config;
+});
+
+export { api };
