8e4a50e68f77300852d7c8f2f72ff13143269b49
alexandresantosm <amenezes031@gmail.com>
2021-12-21 20:30:44 -0300

    feat: add typing

--- app/src/services/resources/user/me-user-usecase.ts
+++ app/src/services/resources/user/me-user-usecase.ts
@@ -1,5 +1,15 @@
 import { api } from "../../api";
 
+interface UserResponse {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  accountNumber: number;
+  accountDigit: number;
+  wallet: number;
+}
+
 export const me = async () => {
-  return api.get("user/me");
+  return api.get<UserResponse>("user/me");
 };
