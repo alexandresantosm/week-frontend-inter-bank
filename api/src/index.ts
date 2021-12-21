20ce48eec0d5101b3876cff2d891aa79574d2b4e
alexandresantosm <amenezes031@gmail.com>
2021-12-21 20:46:52 -0300

    feat: add midlleware cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { createConnection } from 'typeorm';
+import cors from 'cors';
+
 import { globalErrors } from './middlewares/globalErrors';
 import { routes } from './routes';
 
@@ -9,6 +11,7 @@ createConnection()
     const app = express();
     const PORT = 3333;
 
+    app.use(cors());
     app.use(express.json());
     app.use(routes);
     app.use(globalErrors);
