ca8a06419e79a49f5dab41d09cec1e997df13f76
alexandresantosm <amenezes031@gmail.com>
2021-12-28 18:14:57 -0300

    feat: getAllTransactions

--- app/src/pages/Dashboard/Statement/index.tsx
+++ app/src/pages/Dashboard/Statement/index.tsx
@@ -1,17 +1,30 @@
-import { data } from "../../../database/data";
-import { StatementItem } from "./StatementItem";
+import { useEffect, useState } from "react";
+import { StatementItem, StatementItemProps } from "./StatementItem";
 import { StatementContainer } from "./styles";
+import { transactions } from "../../../services/resources/transaction/transactions-usecase";
 
 export const Statement = () => {
-  const statements = data;
+  const [statements, setStatements] = useState<StatementItemProps[]>([]);
+  const getAllTransactions = async () => {
+    const { data } = await transactions();
+    setStatements(data);
+  };
+
+  useEffect(() => {
+    getAllTransactions();
+  }, []);
   return (
     <StatementContainer>
-      {statements.map((statement, index) => (
-        <StatementItem
-          {...statement}
-          key={`${statement.user.firstName}-${index}`}
-        />
-      ))}
+      {statements ? (
+        statements.map((statement, index) => (
+          <StatementItem
+            {...statement}
+            key={`${statement.user.firstName}-${index}`}
+          />
+        ))
+      ) : (
+        <span>Não há transações</span>
+      )}
     </StatementContainer>
   );
 };
