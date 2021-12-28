baced72577d5d562c54ea6b4a7affdd8e5821ead
alexandresantosm <amenezes031@gmail.com>
2021-12-28 18:18:42 -0300

    feat: generate key PIX and pay PIX

--- app/src/pages/Dashboard/index.tsx
+++ app/src/pages/Dashboard/index.tsx
@@ -1,6 +1,10 @@
+import { useState, useEffect } from "react";
 import { Button, Card, Header, Input } from "../../components";
 import { Statement } from "../Dashboard/Statement";
 import { currencyFormatter } from "../../utils/Formatters";
+import { useAuth } from "../../hooks/useAuth";
+import { request } from "../../services/resources/transaction/request-transaction-usecase";
+import { pay } from "../../services/resources/transaction/pay-transaction-usecase";
 
 import {
   DashboardBackground,
@@ -10,8 +14,44 @@ import {
 } from "./styles";
 
 export const Dashboard = () => {
-  const wallet = 5000;
+  const { user, getCurrentUser } = useAuth();
+  const { wallet } = user;
   const walletFormatted = currencyFormatter(wallet);
+  const [value, setValue] = useState("");
+  const [key, setKey] = useState("");
+  const [generatedKey, setGeneratedKey] = useState("");
+
+  const handleNewPayment = async () => {
+    const { data } = await request(Number(value));
+
+    if (data.copyPasteKey) {
+      setGeneratedKey(data.copyPasteKey);
+    }
+  };
+  const handlePayPix = async () => {
+    try {
+      const { data } = await pay(key);
+
+      if (data.msg) {
+        alert(data.msg);
+        return;
+      }
+
+      alert("Não foi possível fazer o pagamento.");
+    } catch (error) {
+      console.log(error);
+      alert("Não é possível receber PIX do mesmo usuário");
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <DashboardBackground>
       <Header />
@@ -30,17 +70,33 @@ export const Dashboard = () => {
               <h2 className="h2">Receber PIX</h2>
             </InlineTitle>
             <InlineContainer>
-              <Input style={{ flex: 1 }} placeholder="Insira o valor R$" />
-              <Button>Gerar Código</Button>
+              <Input
+                style={{ flex: 1 }}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Insira o valor R$"
+              />
+              <Button onClick={handleNewPayment}>Gerar Código</Button>
             </InlineContainer>
+            {generatedKey && (
+              <>
+                <p className="primary-color">Pix copia e cola</p>
+                <p className="primary-color">{generatedKey}</p>
+              </>
+            )}
           </Card>
           <Card noShadow width="90%">
             <InlineTitle>
               <h2 className="h2">Realizar PIX</h2>
             </InlineTitle>
             <InlineContainer>
-              <Input style={{ flex: 1 }} placeholder="Insira a chave PIX" />
-              <Button>Pagar PIX</Button>
+              <Input
+                style={{ flex: 1 }}
+                value={key}
+                onChange={(e) => setKey(e.target.value)}
+                placeholder="Insira a chave PIX"
+              />
+              <Button onClick={handlePayPix}>Pagar PIX</Button>
             </InlineContainer>
           </Card>
         </section>
