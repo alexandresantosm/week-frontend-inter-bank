e07a15ce803dcd42d35a3924e7161e24b5d04354
alexandresantosm <amenezes031@gmail.com>
2021-12-28 18:11:23 -0300

    feat: render profile loading api

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,10 +1,26 @@
 import { useNavigate } from "react-router-dom";
 
+import { useAuth } from "../../hooks/useAuth";
+import {
+  generateFullname,
+  generateFullNumberAccount,
+  generateNameInitials,
+} from "../../utils";
+
 import { Logo, UserCircle } from "../index";
 import { HeaderContainer, HeaderWrapper, UserInfo } from "./styles";
 
 export const Header = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
+  const { firstName, lastName, accountNumber, accountDigit } = user;
+  const fullName = generateFullname(firstName, lastName);
+  const fullNumberAccount = generateFullNumberAccount(
+    accountNumber,
+    accountDigit
+  );
+  const initials = generateNameInitials(firstName, lastName);
+
   const handleLogOff = () => {
     navigate("/");
   };
@@ -14,12 +30,12 @@ export const Header = () => {
       <HeaderWrapper>
         <Logo alt="Logo Inter" height={61} width={172} />
         <UserInfo>
-          <UserCircle initials="AS" />
+          <UserCircle initials={initials} />
           <div>
             <p>
-              Olá, <span className="primary-color font-bold">Alexandre</span>
+              Olá, <span className="primary-color font-bold">{fullName}</span>
             </p>
-            <strong>22001123-1</strong>
+            <strong>{fullNumberAccount}</strong>
             <a href="#" onClick={handleLogOff}>
               Sair
             </a>
